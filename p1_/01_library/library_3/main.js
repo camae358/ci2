da45a1db5093e3241fed58853aa5d5c72479dd91
camae358 <144808205+camae358@users.noreply.github.com>
2024-03-07 07:41:08 -0500

    test update

--- p1_/01_library/library_3/main.js
+++ p1_/01_library/library_3/main.js
@@ -1,24 +1,49 @@
 document.addEventListener("DOMContentLoaded", function() {
     // grid setup
     const gridContainer = document.querySelector(".grid-container");
-    const numberOfSquares = 1000000;
+    const numberOfSquares = 500;
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    const randomIndex = Math.floor(Math.random() * numberOfSquares);
+    // blue box check
+    let blueBoxAssigned = false;
 
-    // grid generation
+    // grid properties
     for (let i = 0; i < numberOfSquares; i++) {
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
 
-    // blue box  
-        if (i === randomIndex) {
-            gridItem.classList.add("blue");
+        // character selection
+        const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
+
+        // grid content
+        gridItem.textContent = randomCharacter;
+
+        // randomize scale
+        const randomScale = Math.random() * 0.5 + 0.75; // Random scale between 0.75 and 1.25
+        gridItem.style.transform = `scale(${randomScale})`;
+
+        // blue box rng
+        if (!blueBoxAssigned && Math.random() < 0.05) { // 5% chance to assign blue box
+            gridItem.classList.add("blue", "no-animation");
             const link = document.createElement("a");
-            link.href = "https://camae358.github.io/ci2/p1_/02_shelves/shelf_3/index.html";
+            link.href = "https://camae358.github.io/ci2/p1_/02_shelves/shelf_1/index.html";
             link.appendChild(gridItem);
             gridContainer.appendChild(link);
+            blueBoxAssigned = true;
         } else {
             gridContainer.appendChild(gridItem);
         }
+    }
+
+    // blue box failsafe
+    if (!blueBoxAssigned) {
+        const randomIndex = Math.floor(Math.random() * numberOfSquares);
+        const gridItems = document.querySelectorAll(".grid-item");
+        const blueBox = gridItems[randomIndex];
+        blueBox.classList.add("blue", "no-animation");
+        const link = document.createElement("a");
+        link.href = "https://camae358.github.io/ci2/p1_/02_shelves/shelf_1/index.html";
+        link.appendChild(blueBox);
+        gridContainer.appendChild(link);
     }
 });
--- p1_/03_text/text_1/main.js
+++ p1_/03_text/text_1/main.js
@@ -40,3 +40,4 @@ document.querySelector('.pill-button').addEventListener('click', function() {
 // button link
     window.location.href = 'https://camae358.github.io/ci2/p1_/01_library/library_2/index.html';
 });
+
