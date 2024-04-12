bdcc69225b1bc351edcb556e6258e927a26071c0
camae358 <144808205+camae358@users.noreply.github.com>
2024-04-12 06:38:08 -0400

    project 2

---
+++ p2_/script.js
@@ -0,0 +1,485 @@
+//destiny
+const destinyImages = [
+    { 
+        "title": "the_traveler",
+        "img": "img/destiny_12.JPG",
+        "franchise": "destiny",
+        "type": "key_visual",
+        "artist": "dorje_bellbrook",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "the_fallen",
+        "img": "img/destiny_7.JPG",
+        "franchise": "destiny",
+        "type": "key_visual",
+        "artist": "jaime_jones",
+        "date": "",
+        "tools": "traditional",
+    },
+
+    { 
+        "title": "old_russia",
+        "img": "img/destiny_8.JPG",
+        "franchise": "destiny",
+        "type": "environment",
+        "artist": "jaime_jones",
+        "date": "",
+        "tools": "traditional",
+    },
+
+    { 
+        "title": "vex_battle",
+        "img": "img/destiny_6.JPG",
+        "franchise": "destiny",
+        "type": "key_visual",
+        "artist": "dorje_bellbrook",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+
+    { 
+        "title": "the_bridge_of_chains",
+        "img": "img/destiny_2.JPG",
+        "franchise": "destiny",
+        "type": "environment",
+        "artist": "jaime_jones",
+        "date": "",
+        "tools": "traditional",
+    },
+
+    { 
+        "title": "the_chasm",
+        "img": "img/destiny_3.JPG",
+        "franchise": "destiny",
+        "type": "environment",
+        "artist": "dorje_bellbrook",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "the_black_garden",
+        "img": "img/destiny_4.JPG",
+        "franchise": "destiny",
+        "type": "environment",
+        "artist": "dorje_bellbrook",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "destiny9",
+        "img": "img/destiny_9.JPG",
+        "franchise": "destiny",
+        "type": "lorem",
+        "artist": "lorem",
+        "date": "lorem",
+        "tools": "lorem",
+    },
+
+    { 
+        "title": "golgoroth",
+        "img": "img/destiny_10.JPG",
+        "franchise": "destiny",
+        "type": "key_visual",
+        "artist": "jaime_jones",
+        "date": "",
+        "tools": "traditional",
+    },
+
+    { 
+        "title": "saladin",
+        "img": "img/destiny_11.JPG",
+        "franchise": "destiny",
+        "type": "key_visual",
+        "artist": "jaime_jones",
+        "date": "",
+        "tools": "traditional",
+    },
+
+    { 
+        "title": "cabal_battle",
+        "img": "img/destiny_1.JPG",
+        "franchise": "destiny",
+        "type": "key_visual",
+        "artist": "jaime_jones",
+        "date": "",
+        "tools": "traditional",
+    },
+
+    { 
+        "title": "the_last_city",
+        "img": "img/destiny_13.JPG",
+        "franchise": "destiny",
+        "type": "environment",
+        "artist": "mark_goldsworthy",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "the_cabal",
+        "img": "img/destiny_14.JPG",
+        "franchise": "destiny",
+        "type": "key_visual",
+        "artist": "jaime_jones",
+        "date": "",
+        "tools": "traditional",
+    },
+]
+
+//dune
+const duneImages = [
+    { 
+        "title": "duncan_idaho_arrives_on_arrakis",
+        "img": "img/dune_10.JPG",
+        "franchise": "dune",
+        "type": "key_visual",
+        "artist": "jeremy_hanna",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "crashed_ornithopter",
+        "img": "img/dune_2.JPG",
+        "franchise": "dune",
+        "type": "key_visual",
+        "artist": "jeremy_hanna",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "worm_attack",
+        "img": "img/dune_5.JPG",
+        "franchise": "dune",
+        "type": "key_visual",
+        "artist": "jeremy_hanna",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+
+    { 
+        "title": "bene_gesserit",
+        "img": "img/dune_1.JPG",
+        "franchise": "dune",
+        "type": "key_visual",
+        "artist": "keith_christensen",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "bene_gesserit_arrive",
+        "img": "img/dune_3.JPG",
+        "franchise": "dune",
+        "type": "key_visual",
+        "artist": "george_hull",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "worm_chase",
+        "img": "img/dune_6.JPG",
+        "franchise": "dune",
+        "type": "key_visual",
+        "artist": "kamen_anev",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "arrival_of_the_guild",
+        "img": "img/dune_7.JPG",
+        "franchise": "dune",
+        "type": "key_visual",
+        "artist": "rodeo",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "sparring",
+        "img": "img/dune_8.JPG",
+        "franchise": "dune",
+        "type": "key_visuals",
+        "artist": "keith_christensen",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "ornithopter",
+        "img": "img/dune_9.JPG",
+        "franchise": "dune",
+        "type": "vehicle",
+        "artist": "george_hull",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "spice_harvester",
+        "img": "img/dune_12.JPG",
+        "franchise": "dune",
+        "type": "vehicle",
+        "artist": "george_hull",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "harkonen_spice_harvester",
+        "img": "img/dune_11.JPG",
+        "franchise": "dune",
+        "type": "vehicle",
+        "artist": "george_hull",
+        "date": "",
+        "tools": "photoshop",
+    },
+]
+
+//tron
+const tronImages = [
+    { 
+        "title": "light_cycle_1",
+        "img": "img/tron_8.JPG",
+        "franchise": "tron",
+        "type": "key_visual",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "the_fall",
+        "img": "img/tron_5.JPG",
+        "franchise": "tron",
+        "type": "key_visual",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "light_cycle_race",
+        "img": "img/tron_3.JPG",
+        "franchise": "tron",
+        "type": "key_visual",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "throne_ship",
+        "img": "img/tron_4.JPG",
+        "franchise": "tron",
+        "type": "vehicle",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "flynn_lightcycle",
+        "img": "img/tron_2.JPG",
+        "franchise": "tron",
+        "type": "vehiclel",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "solar_sailer",
+        "img": "img/tron_6.JPG",
+        "franchise": "tron",
+        "type": "key_visual",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "refuge",
+        "img": "img/tron_7.JPG",
+        "franchise": "tron",
+        "type": "environment",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "lightcycle",
+        "img": "img/tron_1.JPG",
+        "franchise": "tron",
+        "type": "vehicle",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "disc_games_1",
+        "img": "img/tron_9.JPG",
+        "franchise": "tron",
+        "type": "key_visual",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "city",
+        "img": "img/tron_10.JPG",
+        "franchise": "tron",
+        "type": "key_visual",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "rectifier",
+        "img": "img/tron_11.JPG",
+        "franchise": "tron",
+        "type": "key_visual",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "disc_games_2",
+        "img": "img/tron_12.JPG",
+        "franchise": "tron",
+        "type": "key_visual",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+    { 
+        "title": "light_runner",
+        "img": "img/tron_13.JPG",
+        "franchise": "tron",
+        "type": "key_visual",
+        "artist": "vyle_art",
+        "date": "",
+        "tools": "photoshop",
+    },
+
+];
+
+// function to create image elements for destiny
+function createDestinyImageElements() {
+    destinyImages.forEach((image, index) => {
+        const img = document.createElement('div');
+        img.style.backgroundImage = `url(${image.img})`;
+        img.style.backgroundSize = 'cover';
+        img.style.backgroundPosition = 'center';
+        img.style.width = '100%';
+        img.style.height = '100%';
+        img.style.borderRadius = '15px';
+
+        const box = document.querySelector(`.destiny_container .box:nth-child(${index + 1})`);
+        box.innerHTML = '';
+        box.appendChild(img);
+    });
+}
+// call the function to create destiny images
+createDestinyImageElements();
+
+
+// function to create image elements for dune
+function createDuneImageElements() {
+    duneImages.forEach((image, index) => {
+        const img = document.createElement('div');
+        img.style.backgroundImage = `url(${image.img})`;
+        img.style.backgroundSize = 'cover';
+        img.style.backgroundPosition = 'center';
+        img.style.width = '100%';
+        img.style.height = '100%';
+        img.style.borderRadius = '15px';
+
+        const box = document.querySelector(`.dune_container .box:nth-child(${index + 1})`);
+        box.innerHTML = '';
+        box.appendChild(img);
+    });
+}
+// call the function to create dune images
+createDuneImageElements();
+
+// function to create image elements for tron
+function createTronImageElements() {
+    tronImages.forEach((image, index) => {
+        const img = document.createElement('div');
+        img.style.backgroundImage = `url(${image.img})`;
+        img.style.backgroundSize = 'cover';
+        img.style.backgroundPosition = 'center';
+        img.style.width = '100%';
+        img.style.height = '100%';
+        img.style.borderRadius = '15px';
+
+        const box = document.querySelector(`.tron_container .box:nth-child(${index + 1})`);
+        box.innerHTML = '';
+        box.appendChild(img);
+    });
+}
+// call the function to create tron images
+createTronImageElements();
+
+
+// filter images by attribute
+function filterImages(attribute, value) {
+    const filteredImages = imageUrls.filter(image => image[attribute] === value);
+    createImageElements(filteredImages);
+}
+
+//filter gallery
+// Function to filter images by type and display in the gallery
+function filterImagesByType(type, containerClass) {
+    const container = document.querySelector(`.${containerClass}`);
+    const filteredImages = window[`${containerClass}_images`].filter(image => image.type === type);
+
+    // Clear the existing content in the gallery
+    container.querySelector('.gallery').innerHTML = '';
+
+    // Create and append image elements for filtered images
+    filteredImages.forEach(image => {
+        const img = document.createElement('div');
+        img.style.backgroundImage = `url(${image.img})`;
+        img.style.backgroundSize = 'cover';
+        img.style.backgroundPosition = 'center';
+        img.style.width = '100%';
+        img.style.height = '100%';
+        img.style.borderRadius = '15px';
+
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.appendChild(img);
+        container.querySelector('.gallery').appendChild(box);
+    });
+}
+
+// Attach event listeners to filter buttons
+document.querySelectorAll('.pill-button').forEach(button => {
+    button.addEventListener('click', function() {
+        // Toggle the active class on the clicked button
+        this.classList.toggle('active');
+
+        const type = this.textContent.toUpperCase(); // Assuming button text matches the image type
+        const containerClass = this.closest('.container').classList[0]; // Get the container class
+        filterImagesByType(type, containerClass);
+    });
+});
+
